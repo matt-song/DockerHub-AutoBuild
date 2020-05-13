455a43a6f81ee5c6d211f411cf7c78e1285ee39e
Matt Song <matt.song@live.cn>
2020-05-13 01:35:23 +0000

    Updated at 2020-05-13, comments: [no comment, general update]

---
+++ git_tools/git_commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+msg=$1;
+[ x"$msg" == x ] && msg+="no comment, general update"
+DATE_NOW=`date +%F`
+
+cd /data/DockerHub-AutoBuild
+git add *
+git commit -a -m "Updated at $DATE_NOW, comments: [$msg]"
+git push -u origin master
